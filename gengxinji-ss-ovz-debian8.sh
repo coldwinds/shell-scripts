78431a095b5524426776b9cbddf3c4f5239fb1c2
Kernel Jackson <coldwinds@users.noreply.github.com>
2018-01-27 13:42:28 +0800

    Update gengxinji-ss-ovz-debian8.sh

--- gengxinji-ss-ovz-debian8.sh
+++ gengxinji-ss-ovz-debian8.sh
@@ -6,9 +6,11 @@ apt-get -y update
 apt-get -y install php5-cli
 
 # install lkl
-wget --no-check-certificate https://github.com/91yun/uml/raw/master/lkl/install.sh && bash install.sh && ping 10.0.0.2
+wget --no-check-certificate https://github.com/91yun/uml/raw/master/lkl/install.sh && bash install.sh
 
 # install shadowsocks-libev-debian
 wget --no-check-certificate -O shadowsocks-libev-debian.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-libev-debian.sh
 chmod +x shadowsocks-libev-debian.sh
 ./shadowsocks-libev-debian.sh 2>&1 | tee shadowsocks-libev-debian.log
+
+ping 10.0.0.2
